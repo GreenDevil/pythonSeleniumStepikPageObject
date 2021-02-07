bfd556681d0f6f9616b7470d43bfc23f6df82005
Evgeniy <e.dultzev@gmail.com>
2021-02-07 15:47:23 +0300

    add files from Section 4 Lesson 3 Step 2, 3, 4
    add function "get_text_from_element", "solve_quiz_and_get_code" to base_page.py
    
    add ProductPageLocators
    
    add product_page.py class and functions
    
    add tests for Product page

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,11 @@ class LoginPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_form")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_link")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+    BUTTON_ADD_TO_BASKET = (By.CLASS_NAME, "btn-add-to-basket")
+    PRODUCT_NAME_H1 = (By.CSS_SELECTOR, ".product_main h1")
+    ALERT_SUCCESS = (By.CLASS_NAME, "alertinner")
+    MAIN_PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    BASKET = (By.CLASS_NAME, "basket-mini")
